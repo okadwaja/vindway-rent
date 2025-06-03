6f4d54818e7e30acb5854bc1be0cb265cfa620c1
okadwaja <okadwaja99@gmail.com>
2025-06-03 12:37:56 +0800

    first commit

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-lg-6">
                     <div class="card">
 
-                        <form action="{{ route('profile.update') }}" method="POST">
+                        <form action="{{ route('admin.profile.update') }}" method="POST">
                             @csrf
                             @method('PUT')
 
